225d6b03e05abe13ff73d9308e8f740ad97d8a1d
MalfinK <muhammadalfin1235678@gmail.com>
2024-06-14 21:23:35 +0700

    benerin prefix

--- api/config.php
+++ api/config.php
@@ -4,5 +4,5 @@ require_once '../vendor/autoload.php';
 use EasyRdf\Sparql\Client;
 
 // Buat koneksi ke server SPARQL menggunakan EasyRDF
-$endpoint = new Client('http://localhost:3030/jartik');
+$endpoint = new Client('http://localhost:3030/jartik2');
 ?>
--- api/dosen.php
+++ api/dosen.php
@@ -4,18 +4,29 @@ require_once 'config.php';
 // Fungsi untuk mendapatkan semua data dosen
 function getDataDosen($endpoint, $action = null) {
     $query = "
-        PREFIX ex: <http://www.semanticweb.org/mak/ontologies/2024/4/jartik#>
-        SELECT ?nip ?namaDosen ?statusKeaktifanDosen ?unitKerjaDosen
+    PREFIX all:<http://www.semanticweb.org/aditf/ontologies/2024/4/main-ontology-jejaring-semantik#>
+    PREFIX d:<http://www.semanticweb.org/mak/ontologies/2024/4/jartik#>
+    PREFIX k:<http://www.semanticweb.org/kenneth/ontologies/2024/4/mahasiswa#>
+        SELECT ?nip ?namaDosen ?statusKeaktifanDosen ?unitKerjaDosen ?namaMahasiswa
         WHERE {
-            ?dosen ex:nip ?nip ;
-                   ex:namaDosen ?namaDosen ;
-                   ex:statusKeaktifanDosen ?statusKeaktifanDosen ;
-                   ex:unitKerjaDosen ?unitKerjaDosen .
+            ?dosen d:nip ?nip ;
+                   d:namaDosen ?namaDosen ;
+                   d:statusKeaktifanDosen ?statusKeaktifanDosen ;
+                   d:unitKerjaDosen ?unitKerjaDosen ;
+                   d:mengajar ?mhs ;
+                   d:mengampu ?mk ;
+                   d:membimbing ?prs ;
+                   d:membina ?org ;
+                   d:menghadiri ?jdwl ;
+                   d:mengisi ?nilai ;
+                   d:menyusun ?jdwl2.
+            ?mhs k:namaMahasiswa ?namaMahasiswa.
             FILTER (
                 regex(str(?nip), '$action', 'i') ||
                 regex(str(?namaDosen), '$action', 'i') ||
                 regex(str(?statusKeaktifanDosen), '$action', 'i') ||
-                regex(str(?unitKerjaDosen), '$action', 'i')
+                regex(str(?unitKerjaDosen), '$action', 'i') ||
+                regex(str(?namaMahasiswa), '$action', 'i')
             )
         }
     ";
@@ -28,7 +39,8 @@ function getDataDosen($endpoint, $action = null) {
             'nip' => $row->nip->getValue(),
             'namaDosen' => $row->namaDosen->getValue(),
             'statusKeaktifanDosen' =>$row->statusKeaktifanDosen->getValue(),
-            'unitKerjaDosen' => $row->unitKerjaDosen->getValue()
+            'unitKerjaDosen' => $row->unitKerjaDosen->getValue(),
+            'namaMahasiswa' => $row->namaMahasiswa->getValue()
         ];
     }
 
